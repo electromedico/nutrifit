d08ec1ac1db66c9c998b4b429d2ddf054abd7097
Alex <electromedico@hotmail.com>
2020-03-30 22:27:13 -0400

    modification de paquetes crecion de iniciocontroleer

--- src/main/java/com/mycompany/nutrifitv2/controller/database/dao/DataBaseAcessor.java
+++ src/main/java/com/mycompany/nutrifitv2/controller/database/dao/DataBaseAcessor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.nutrifitv2.controller.dao;
+package com.mycompany.nutrifitv2.controller.database.dao;
 
 /**
  *
